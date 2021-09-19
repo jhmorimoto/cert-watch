da12eee54aa69dc876c6b748a0fc2c274d304e85
Julio Morimoto <julio@morimoto.net.br>
2021-09-18 23:49:49 -0300

    Remove comments

--- controllers/core/secret_controller.go
+++ controllers/core/secret_controller.go
@@ -35,15 +35,6 @@ type SecretReconciler struct {
 //+kubebuilder:rbac:groups=core,resources=secrets,verbs=get;list;watch
 //+kubebuilder:rbac:groups=core,resources=secrets/status,verbs=get
 
-// Reconcile is part of the main kubernetes reconciliation loop which aims to
-// move the current state of the cluster closer to the desired state.
-// TODO(user): Modify the Reconcile function to compare the state specified by
-// the Secret object against the actual cluster state, and then
-// perform operations to make the cluster state reflect the state specified by
-// the user.
-//
-// For more details, check Reconcile and its Result here:
-// - https://pkg.go.dev/sigs.k8s.io/controller-runtime@v0.8.3/pkg/reconcile
 func (r *SecretReconciler) Reconcile(ctx context.Context, req ctrl.Request) (ctrl.Result, error) {
 	klog.Infof("Secret is here %s/%s", req.Namespace, req.Name)
 	return ctrl.Result{}, nil
