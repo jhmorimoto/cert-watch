bd59b286b010d72a33afbf41a83c1e0024e43174
Julio Morimoto <julio@morimoto.net.br>
2021-09-18 23:48:17 -0300

    Remove uncessary permissions from Secret controller.

--- controllers/core/secret_controller.go
+++ controllers/core/secret_controller.go
@@ -32,9 +32,8 @@ type SecretReconciler struct {
 	Scheme *runtime.Scheme
 }
 
-//+kubebuilder:rbac:groups=core,resources=secrets,verbs=get;list;watch;create;update;patch;delete
-//+kubebuilder:rbac:groups=core,resources=secrets/status,verbs=get;update;patch
-//+kubebuilder:rbac:groups=core,resources=secrets/finalizers,verbs=update
+//+kubebuilder:rbac:groups=core,resources=secrets,verbs=get;list;watch
+//+kubebuilder:rbac:groups=core,resources=secrets/status,verbs=get
 
 // Reconcile is part of the main kubernetes reconciliation loop which aims to
 // move the current state of the cluster closer to the desired state.
