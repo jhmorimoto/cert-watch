2d0872577e1a356e0255e71acf57996705799505
Julio Morimoto <julio@morimoto.net.br>
2021-09-27 23:36:37 -0300

    Simple go code reformat.

--- main.go
+++ main.go
@@ -3,7 +3,6 @@ package main
 import (
 	"flag"
 	"os"
-
 	// Import all Kubernetes client auth plugins (e.g. Azure, GCP, OIDC, etc.)
 	// to ensure that exec-entrypoint and run can make use of them.
 	_ "k8s.io/client-go/plugin/pkg/client/auth"
