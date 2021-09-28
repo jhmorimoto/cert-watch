b1e288fa986637b9786361eb1626e20419e49a90
Julio Morimoto <julio@morimoto.net.br>
2021-09-28 16:43:41 -0300

    Move job action to its own function in the util package.

--- controllers/certwatch/certwatcher_controller.go
+++ controllers/certwatch/certwatcher_controller.go
@@ -3,7 +3,6 @@ package certwatch
 import (
 	"context"
 	"fmt"
-	v1 "k8s.io/api/batch/v1"
 	"os"
 	"strings"
 	"time"
@@ -172,62 +171,18 @@ func (r *CertWatcherReconciler) Reconcile(ctx context.Context, req ctrl.Request)
 			}
 		}
 		if certwatcher.Spec.Actions.Job != nil {
-			var job v1.Job
-			var jobname string
-
-			// Default job name
-			if s, err := util.RandoHash(12); err == nil {
-				jobname = certwatcher.Name + "-" + certwatcher.Spec.Actions.Job.Name + "-" + s
-			} else {
-				r.EventRecorder.Eventf(&certwatcher, "Warning", "CertWatcherProcessing", "JOB unable to determine new job name: %s", err.Error())
-				certwatcher.Status.Message = err.Error()
-				return r.updateCertWatcher(ctx, &certwatcher, err)
-			}
-
-			// Default volume name and mountPath
-			if certwatcher.Spec.Actions.Job.VolumeName == "" {
-				certwatcher.Spec.Actions.Job.VolumeName = "certs"
-			}
-			if certwatcher.Spec.Actions.Job.MountPath == "" {
-				certwatcher.Spec.Actions.Job.MountPath = "/workspace"
-			}
-
-			job = v1.Job{
-				ObjectMeta: apimachineryv1.ObjectMeta{
-					Namespace: certwatcher.Namespace,
-					Name:      jobname,
-				},
-				Spec: certwatcher.Spec.Actions.Job.Spec,
-			}
-
-			// Create an additional volume in the pod spec
-			job.Spec.Template.Spec.Volumes = append(job.Spec.Template.Spec.Volumes, apicorev1.Volume{
-				Name: certwatcher.Spec.Actions.Job.VolumeName,
-				VolumeSource: apicorev1.VolumeSource{
-					Secret: &apicorev1.SecretVolumeSource{
-						SecretName: certwatcher.Spec.Secret.Name,
-					},
-				},
-			})
-
-			// Create a volumeMount in each container in the pod spec
-			for i := range job.Spec.Template.Spec.Containers {
-				job.Spec.Template.Spec.Containers[i].VolumeMounts = append(job.Spec.Template.Spec.Containers[i].VolumeMounts,
-					apicorev1.VolumeMount{
-						Name:      certwatcher.Spec.Actions.Job.VolumeName,
-						MountPath: certwatcher.Spec.Actions.Job.MountPath,
-					},
-				)
-			}
-
-			// Create the job
-			err = r.Create(ctx, &job)
+			job, err := util.ProcessJob(&certwatcher)
 			if err != nil {
-				r.EventRecorder.Eventf(&certwatcher, "Warning", "CertWatcherProcessing", "JOB: %s", err.Error())
+				r.EventRecorder.Eventf(&certwatcher, "Warning", "CertWatcherProcessing", "JOB Error preparing new job: %s", err.Error())
 				certwatcher.Status.Message = err.Error()
 				return r.updateCertWatcher(ctx, &certwatcher, err)
 			}
-
+			err = r.Create(ctx, job)
+			if err != nil {
+				r.EventRecorder.Eventf(&certwatcher, "Warning", "CertWatcherProcessing", "JOB: Error creating new job%s", err.Error())
+				certwatcher.Status.Message = err.Error()
+				return r.updateCertWatcher(ctx, &certwatcher, err)
+			}
 		}
 
 		certwatcher.Status.ActionStatus = "Ready"
---
+++ controllers/util/process_job.go
@@ -0,0 +1,59 @@
+package util
+
+import (
+	"fmt"
+	certwatchv1 "github.com/jhmorimoto/cert-watch/apis/certwatch/v1"
+	v1 "k8s.io/api/batch/v1"
+	apicorev1 "k8s.io/api/core/v1"
+	apimachineryv1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+func ProcessJob(certwatcher *certwatchv1.CertWatcher) (*v1.Job, error) {
+	var job v1.Job
+	var jobname string
+
+	// Default job name
+	if s, err := RandoHash(12); err == nil {
+		jobname = certwatcher.Name + "-" + certwatcher.Spec.Actions.Job.Name + "-" + s
+	} else {
+		return nil, fmt.Errorf("unable to determine new job name: %s", err.Error())
+	}
+
+	// Default volume name and mountPath
+	if certwatcher.Spec.Actions.Job.VolumeName == "" {
+		certwatcher.Spec.Actions.Job.VolumeName = "certs"
+	}
+	if certwatcher.Spec.Actions.Job.MountPath == "" {
+		certwatcher.Spec.Actions.Job.MountPath = "/workspace"
+	}
+
+	job = v1.Job{
+		ObjectMeta: apimachineryv1.ObjectMeta{
+			Namespace: certwatcher.Namespace,
+			Name:      jobname,
+		},
+		Spec: certwatcher.Spec.Actions.Job.Spec,
+	}
+
+	// Create an additional volume in the pod spec
+	job.Spec.Template.Spec.Volumes = append(job.Spec.Template.Spec.Volumes, apicorev1.Volume{
+		Name: certwatcher.Spec.Actions.Job.VolumeName,
+		VolumeSource: apicorev1.VolumeSource{
+			Secret: &apicorev1.SecretVolumeSource{
+				SecretName: certwatcher.Spec.Secret.Name,
+			},
+		},
+	})
+
+	// Create a volumeMount in each container in the pod spec
+	for i := range job.Spec.Template.Spec.Containers {
+		job.Spec.Template.Spec.Containers[i].VolumeMounts = append(job.Spec.Template.Spec.Containers[i].VolumeMounts,
+			apicorev1.VolumeMount{
+				Name:      certwatcher.Spec.Actions.Job.VolumeName,
+				MountPath: certwatcher.Spec.Actions.Job.MountPath,
+			},
+		)
+	}
+
+	return &job, nil
+}
